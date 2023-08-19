1d092a564dcac08173aed2cc6898ff4ad602ea87
inkoziev <kelijah@yandex.ru>
2023-08-19 07:09:34 +0300

    making the project installable via pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+import io
+import setuptools
+
+setuptools.setup(
+    name="charactertokenizer",
+    version="1.0.0",
+    author="dariush-bahrami, Ilya Koziev",
+    author_email="inkoziev@gmail.com",
+    description="Character-level tokenizer for charGPT and charLLaMa projects",
+    url="https://github.com/Koziev/character-tokenizer",
+    packages=setuptools.find_packages(),
+    include_package_data=False,
+)
